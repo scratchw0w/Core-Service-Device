376722618b615a943096dab736afe268d1164ea8
Max Basov <maksym.basov@nixsolutions.com>
2021-04-16 16:19:41 +0300

    [core-service] Added kafka support, also added kafka dependency, config and some properties to the application.properties.

--- src/main/java/com/scratchy/service/impl/DeviceServiceImpl.java
+++ src/main/java/com/scratchy/service/impl/DeviceServiceImpl.java
@@ -1,16 +1,26 @@
 package com.scratchy.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scratchy.domain.Device;
 import com.scratchy.repository.DeviceRepository;
 import com.scratchy.service.DeviceService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Service
+@Log4j2
+@EnableKafka
 public class DeviceServiceImpl implements DeviceService {
+
+    private static final String TOPIC_NAME = "device-topic";
 
     @Autowired
     private DeviceRepository deviceRepository;
@@ -59,5 +69,21 @@ public class DeviceServiceImpl implements DeviceService {
         }
 
         return device;
+    }
+
+    @KafkaListener(topics = TOPIC_NAME)
+    void deviceListSaver(String data) {
+        log.info("Getting data from topic: " + TOPIC_NAME);
+        List<Device> deviceList;
+        ObjectMapper mapper = new ObjectMapper();
+        log.info("Deserializing data");
+        try {
+            deviceList = List.of(mapper.readValue(data, Device[].class));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        log.info("Saving data to the database");
+        deviceList.forEach(this::createDevice);
     }
 }
